6fbea329ca13f7fd4574a4edbd4ec88e05883d42
zakhar.bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2023-04-21 08:54:57 +0200

    beginning: a little clock has chacked (21.04.2023)

--- clock/App.js
+++ clock/App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from'react';
+import { StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
+
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+  
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.clock}>{time}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+
+  clock: {
+
+  }
 });
